2f120c21f469fa83c39305b1851e4044b1818c99
dthinkc <66877319+dthinkc@users.noreply.github.com>
2022-02-07 19:45:19 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '36e5fea81cf845d0b9e7d026128804cc',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '科技是第一生产力',
+  domain: 'https://eminent-trout-d5a.notion.site/36e5fea81cf845d0b9e7d026128804cc',
+  author: 'wd',
 
   // open graph metadata (optional)
   description: 'Example site description',
